cbe242d80f5eb65fc122f6640660bf5c16e0a9ab
mischinl <62237454+mischinl@users.noreply.github.com>
2020-03-19 14:34:26 +0100

    Update InsultGenerator.java

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -8,7 +8,7 @@ public class InsultGenerator {
 	public String generateInsult() {
 		String vowels = "AEIOU";
 		String article = "an";
-		String theInsult = "";h
+		String theInsult = "";
 
 		try {
 			String databaseURL = "jdbc:postgresql://";
